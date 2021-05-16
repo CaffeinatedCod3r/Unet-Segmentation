100d5a548a8927e433b7db2cc68381335892e0e7
CaffeinatedCod3r <rishabh.agr197@gmail.com>
2021-05-16 20:26:30 +0530

    Optimized model

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from data import *
 testGene = testGenerator("data/membrane/test")
 model = unet()
 model.load_weights("unet_membrane.hdf5")
-results = model.predict_generator(testGene, 2, verbose=1)
+results = model.predict_generator(testGene, 30, verbose=1)
 
 saveResult("data/membrane/newtest", results)
